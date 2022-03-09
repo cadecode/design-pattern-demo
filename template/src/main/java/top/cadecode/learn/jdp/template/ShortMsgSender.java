ac28d4f9e21446914bfaf891fffb91877ebcd7be
Cade Li <cade.li@qq.com>
2022-03-09 21:57:24 +0800

    feat: 添加模板方法模式代码

---
+++ template/src/main/java/top/cadecode/learn/jdp/template/ShortMsgSender.java
@@ -0,0 +1,24 @@
+package top.cadecode.learn.jdp.template;
+
+/**
+ * @author Cade Li
+ * @date 2022/3/9
+ * @description 短信发送消息
+ */
+public class ShortMsgSender extends MsgSender {
+    @Override
+    protected void check(String msg) {
+        System.out.println("短信发送前检查消息格式");
+    }
+
+    @Override
+    protected void exec(String msg) {
+        System.out.println("短信发送消息中");
+        throw new RuntimeException();
+    }
+
+    @Override
+    protected void error(String msg, Throwable t) {
+        System.out.println("短信发送消息失败：" + msg);
+    }
+}
